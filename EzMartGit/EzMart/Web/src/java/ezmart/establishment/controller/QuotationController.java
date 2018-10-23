0ff5e3782b16629bcffb9095a08617307b6ef0f9
mpmoreno1990 <mpmoreno1990@gamil.com>
2018-10-22 22:05:00 -0300

    Mudanças de contação no controller

--- EzMartGit/EzMart/Web/src/java/ezmart/establishment/controller/QuotationController.java
+++ EzMartGit/EzMart/Web/src/java/ezmart/establishment/controller/QuotationController.java
@@ -1,8 +1,10 @@
 package ezmart.establishment.controller;
 
 import ezmart.model.entity.Establishment;
+import ezmart.model.entity.EstablishmentProduct;
 import ezmart.model.entity.User;
 import ezmart.model.service.EstablishmentService;
+import java.util.ArrayList;
 import java.util.List;
 import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
@@ -51,14 +53,38 @@ public class QuotationController {
 
             if (auxSession instanceof Establishment) {
 
-                mv = new ModelAndView("establishment/quotation");
+                mv = new ModelAndView("establishment/quotation_result");
 
                 user = (Establishment) auxSession;
 
                 EstablishmentService establishmentService = new EstablishmentService();
                 Establishment establishment = establishmentService.readByUserId(user.getId());
 
-                //TODO
+                List<EstablishmentProduct> establishmentProductList = establishmentService.findAllEstablishmentProduct(establishment.getId());
+                if (establishmentProductList != null && !establishmentProductList.isEmpty()) {
+                    establishment.setProductList(establishmentProductList);
+                }
+
+                Establishment competitor = establishmentService.readByEstablishmentId(competitorId);
+
+                List<Long> productIdList = new ArrayList<>();
+                for (EstablishmentProduct product : establishmentProductList) {
+                    productIdList.add(product.getId());
+                }
+
+                List<EstablishmentProduct> competitorList = establishmentService.findAllEstablishmentProductByCompetitorId(competitorId, productIdList);
+                if (competitorList != null && !competitorList.isEmpty()) {
+                    competitor.setProductList(competitorList);
+                }
+
+                List<Establishment> establishmentList = new ArrayList<>();
+                establishmentList.add(establishment);
+                establishmentList.add(competitor);
+
+                mv.addObject("establishmentList", establishmentList);
+                mv.addObject("establishmentProductList", establishmentProductList);
+                mv.addObject("competitorList", competitorList);
+
             }
 
         } catch (Exception exception) {
